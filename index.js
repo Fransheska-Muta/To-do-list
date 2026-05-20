d24f40a0a1484a57ad62dd17801868ee0e9829a5
mutafransheska45-maker <mutafransheska45@gmail.com>
2026-05-20 20:46:17 +0200

    The list is complete

--- index.js
+++ index.js
@@ -1,41 +1,61 @@
 const input = document.getElementById("field");
 const listContainer = document.getElementById("list-container");
 let date = document.getElementById("date"); // getting the value of the date input
+
+
 // this function will be called when the add button is clicked, 
 // it will check if the input is empty, if not it will create a list item and add it to the list container,
 //  it will also add a cross icon for deleting the item and a span for the date
+function addTask() {
 
-function addTask(){
-    if(input.value === ""){
-        alert("Please add a task")
-    }else{
-        let li = document.createElement("li");
-        li.innerHTML = input.value; // displaying the task and due time
-        listContainer.appendChild(li) // displaying the list as a child onto the list container which is the parent
-        saveData()
-        
-        let span1 = document.createElement("span");
-        span1.innerHTML = "\u00d7"; // this is a cross icon
-        li.appendChild(span1);
-        span1.onclick = function(){ 
-        span1.parentElement.remove() // this will remove the list item when the cross icon is clicked
-        saveData()      
+    // Check task input
+    if (input.value === "") {
+        alert("Please add a task");
+        return;
     }
 
-    if(date.value === ""){
-        alert ("Please add a due date")
-    }else{
-        let span2 = document.createElement("span");
-        span2.innerHTML = date.value;
-        li.appendChild(span2);
+    // Check date input
+    if (date.value === "") {
+        alert("Please add a due date");
+        return;
     }
 
-}
-    input.value = "" // so that after adding an item the input is empty, ready for another item
-    saveData()
+    // Create list item only if both inputs are valid
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    listContainer.appendChild(li);
+    saveData();
+
+    // Delete (cross icon)
+    let span1 = document.createElement("span");
+    span1.innerHTML = "\u00d7";
+    li.appendChild(span1);
+
+    // Due date display
+    let span2 = document.createElement("span");
+    span2.innerHTML = date.value;
+    li.appendChild(span2);
+
+    // Clear inputs
+    input.value = "";
+    date.value = "";
+    saveData();
 }
 
-
+// I am saying "pay attention to the listContainer, 
+// if anyhting inside it is clicked, run this function"
+// event delegation, it tells the function what happended and where it happend
+listContainer.addEventListener("click", function(event) {  
+    // Check if the clicked element is a list item (<li>)
+    if (event.target.tagName === "LI") {
+        // Add "checked" class if it's not there, or remove it if it is
+        event.target.classList.toggle("checked"); // toggle means to change between 2 states, checked and unchecked
+    }else if(event.target.tagName === "SPAN") {
+        event.target.parentElement.remove();
+        saveData();
+    }
+    
+});
 
 // saving the data so it does not get lost when refreshed
 function saveData(){
@@ -46,4 +66,4 @@ function saveData(){
 function showData(){
     listContainer.innerHTML = localStorage.getItem("data")
 }
-// showData()
+showData();
